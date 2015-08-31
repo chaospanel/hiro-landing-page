c9b288a7af1796abb8f68873a7d8c52c1a489300
Alex <alemik@live.se>
2015-08-31 13:09:26 +0300

    Landing page

---
+++ index.js
@@ -0,0 +1,23 @@
+var index = (function() {
+  function scaleElementHeight(el) {
+    el.style.height = (window.innerHeight + 5) + 'px';
+  }
+
+  var module = {
+    autoResizeElement: function(querySelector) {
+      var el = document.querySelector(querySelector);
+
+      window.addEventListener('resize', function() {
+        scaleElementHeight(el);
+      });
+
+      scaleElementHeight(el);
+    },
+    toggleElement: function(elementId) {
+      var el = document.getElementById(elementId);
+      el.style.visibility = (el.style.visibility === 'visible') ? 'hidden' : 'visible';
+    }
+  };
+
+  return module;
+}());
